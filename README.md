0ff70a95d6264993ac72d1bec5f95467c0a707ae
vikat <vikatthakur007@gmail.com>
2025-09-27 20:46:24 +0530

    zoomcamp_week_1_etl_sql

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # de-zoomcamp
 Data Engineering A-Z Course
+
+dpage/pgadmin4
+# Run Postgres in CMD on Windows
+
+```powershell
+docker run -it \ 
+  -e POSTGRES_USER="root" \ 
+  -e POSTGRES_PASSWORD="root" \ 
+  -e POSTGRES_DB="ny_taxi" \ 
+  -v C:/Users/vikat/OneDrive/Desktop/NotionAi/de-zoomcamp/ny_taxi_postgres_data:/var/lib/postgresql/data \ 
+  -p 5432:5432 \ 
+  postgres:13
+```
+
+## Run Postgres and pgAdmin in the same Docker network
+
+### 1. Create the network
+```powershell
+docker network create pg_network
+```
+
+### 2. Run the Postgres container
+```powershell
+docker run -d \ 
+  --name postgres \ 
+  --network pg_network \ 
+  -e POSTGRES_USER=root \ 
+  -e POSTGRES_PASSWORD=root \ 
+  -e POSTGRES_DB=ny_taxi \ 
+  -v C:/Users/vikat/OneDrive/Desktop/NotionAi/de-zoomcamp/ny_taxi_postgres_data:/var/lib/postgresql/data \ 
+  -p 5432:5432 \ 
+  postgres:15
+```
+
+### 3. Run the pgAdmin container
+```powershell
+docker run -d \ 
+  --name pgadmin \ 
+  --network pg_network \ 
+  -e PGADMIN_DEFAULT_EMAIL=admin@admin.com \ 
+  -e PGADMIN_DEFAULT_PASSWORD=admin \ 
+  -p 5050:80 \ 
+  dpage/pgadmin4
+```
+
--- etl_postgres.py
+++ etl_postgres.py
@@ -9,55 +9,93 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, date_format
 from pyspark.sql.types import TimestampType
+from sqlalchemy import create_engine
+from time import time
 
 import pandas as pd
+import os
+import argparse
 
-spark = SparkSession.builder.master("local[*]").appName("postgres_upload_ny_taxi").getOrCreate()
 
-# read the parquet data from /data folder into a dataframe
-df = spark.read.parquet("data/ny_taxi_data_yellow/yellow_tripdata_2021-01.parquet")
+def main(params):
+    ''''
+    # Example credentials
+        username = "root"
+        password = "root"
+        host = "localhost"
+        port = 5432
+        database = "ny_taxi"
+    '''
 
-# Convert timestamp columns to string first
-timestamp_cols = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
-for c in timestamp_cols:
-    df = df.withColumn(c, date_format(col(c), "yyyy-MM-dd HH:mm:ss"))
+    username = params.username
+    password = params.password
+    host = params.host
+    port = params.port
+    database = params.database
 
-# volume of data
-print("Row count:", df.count())
+    output_file_path = "data/output.parquet"
 
-# data showcase/ schema showcase
-df.printSchema()
+    #download the from os wget command
+    os.system(f"wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-02.parquet -O {output_file_path}"
+    )
 
-# convert to pandas
-pdf = df.toPandas()
-for c in timestamp_cols:
-    pdf[c] = pd.to_datetime(pdf[c])
+    spark = SparkSession.builder.master("local[*]").appName("etl-postgres-1").getOrCreate()
 
-from sqlalchemy import create_engine
-# Example credentials
-username = "root"
-password = "root"
-host = "localhost"
-port = 5432
-database = "ny_taxi"
+    # read the parquet data from /data folder into a dataframe
+    df = spark.read.parquet(output_file_path)
 
-# Create the engine
-engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
-engine.connect()
+    # Convert timestamp columns to string first
+    timestamp_cols = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+    for c in timestamp_cols:
+        df = df.withColumn(c, date_format(col(c), "yyyy-MM-dd HH:mm:ss"))
 
-create_statement_postgres = pd.io.sql.get_schema(pdf, name="yellow_tax_data", con=engine)
-print(create_statement_postgres)
+    # volume of data
+    print("Row count for downloaded file:", df.count())
 
-# creating and inserting data in yellow_taxi_data
-# create yellow_taxi_data
-df_head = pdf.head(0)
-df_head.to_sql(name="yellow_taxi_date", con=engine, if_exists="append")
+    # data showcase/ schema showcase
+    df.printSchema()
 
-# insert chunks of 10000
-from time import time
-chunksize = 10000
-for i in range(0, len(pdf), chunksize):
-    t_start = time()
-    pdf.iloc[i:i+chunksize].to_sql(name="yellow_taxi_date", con=engine, if_exists="append")
-    t_end = time()
-    print(f"Inserted data successfully in {t_end - t_start:.3f}s")
+    # convert to pandas
+    pdf = df.toPandas()
+    for c in timestamp_cols:
+        pdf[c] = pd.to_datetime(pdf[c])
+
+    # Create the engine
+    engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
+    engine.connect()
+
+    create_statement_postgres = pd.io.sql.get_schema(pdf, name="yellow_tax_data", con=engine)
+    print(create_statement_postgres)
+
+    # creating and inserting data in yellow_taxi_trips
+    # create yellow_taxi_trips
+    table_name = "yellow_taxi_trips"
+    df_head = pdf.head(0)
+    df_head.to_sql(name=table_name, con=engine, if_exists="append")
+
+    # insert chunks of 10000
+
+    chunksize = 10000
+    for i in range(0, len(pdf), chunksize):
+        t_start = time()
+        pdf.iloc[i:i+chunksize].to_sql(name=table_name, con=engine, if_exists="append")
+        t_end = time()
+        print(f"Inserted data successfully in {t_end - t_start:.3f}s")
+    print("All data inserted successfully")
+
+
+
+
+
+if __name__ == "__main__":
+
+#argparse all above parameters
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--username", help="username for postgres", default="root")
+    argparser.add_argument("--password", help="password for postgres", default="root")
+    argparser.add_argument("--host", help="host for postgres", default="localhost")
+    argparser.add_argument("--port", help="port for postgres", default=5432)
+    argparser.add_argument("--database", help="database for postgres", default="ny_taxi")
+    args = argparser.parse_args()
+    main(args)
+
